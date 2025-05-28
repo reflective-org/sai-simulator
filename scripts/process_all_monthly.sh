04a843f5df3478d9d3cfab277eb3480019612149
Ali Akherati <aakherati91@gmail.com>
2025-05-28 12:16:47 -0700

    added lines

--- scripts/process_all_monthly.sh
+++ scripts/process_all_monthly.sh
@@ -6,34 +6,35 @@ fi
 
 echo "Processing tas..."
 python scripts/process_monthly_gauss.py  --var tas --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing pr..."
 python scripts/process_monthly_gauss.py  --var pr --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing tasmax..."
 python scripts/process_monthly_gauss.py  --var tasmax --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing tasmin..."
 python scripts/process_monthly_gauss.py  --var tasmin --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing tas_above_35..."
 python scripts/process_monthly_gauss.py  --var tas_above_35 --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing tas_above_40..."
 python scripts/process_monthly_gauss.py  --var tas_above_40 --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing tas_below_0..."
 python scripts/process_monthly_gauss.py  --var tas_below_0 --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing pr_above_10..."
 python scripts/process_monthly_gauss.py  --var pr_above_10 --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing pr_above_20..."
 python scripts/process_monthly_gauss.py  --var pr_above_20 --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing p-e..."
 python scripts/process_monthly_gauss.py  --var p-e --data_dir $1 --output_dir $2
-
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Processing icefrac..."
 python scripts/process_monthly_gauss.py  --var icefrac --data_dir $1 --output_dir $2
+echo "\n-----------------------------------------------------------------------------------------\n"
 echo "Done."
